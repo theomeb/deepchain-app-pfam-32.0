9f16666304dfa29bb531e380e21eeff3224faea8
Théomé Borck <theome.borck@student.ecp.fr>
2021-05-05 17:10:05 +0200

    Add the app and model/label_encoder checkpoints

--- src/DESC.md
+++ src/DESC.md
@@ -1,7 +1,40 @@
 # Description
-This app is a base application that compute the loglikelihood (the probability that a protein is natural or not).
+**Pfam32.0 classifier.**
 
-We use a bio-transformers with a ESM 'prot_bert' model as backend to make the computation of logits
+This is a **DeepChain app** to predict the _protein family id_ out of a given sequence. 
+
+- This app has been trained with the Pfam32.0 dataset which you can pull 
+with the [bio-datasets](https://pypi.org/project/bio-datasets):
+```python
+# Load pfam dataset
+pfam_dataset = load_dataset("pfam-32.0", force=True)
+_, y = pfam_dataset.to_npy_arrays(input_names=["sequence"], target_names=["family_id"])
+```
+
+- This app uses [ProtBert](https://github.com/agemagician/ProtTrans) to compute embeddings with the `mean` pooling.
+This is done thanks to [bio-transformers](https://pypi.org/project/bio-transformers/)
+  
+- The classifier then computes the protein family id thanks to a Dense classification on top of the sequences' embeddings. 
+
+
+This app is mean to be deployed in deepchain.bio and has been implemented thanks to the following libraries:
+- The main [deepchain-apps](https://pypi.org/project/deepchain-apps/) package - can be found on pypi.
+- The [bio-transformers](https://pypi.org/project/bio-transformers/) package.
+- The [bio-datasets](https://pypi.org/project/bio-datasets) package.
+
+# App structure
+
+- deepchain-app-theome
+  - src/
+    - app.py
+    - DESCRIPTION.md
+    - tags.json
+    - Optionnal : requirements.txt (for extra packages)
+  - checkpoint/
+    - Optionnal : family_model.pt
+    - Optionnal : label_encoder.joblib
+
+The main app class must be named ’App’
 
 # Tags
 
@@ -9,12 +42,15 @@ We use a bio-transformers with a ESM 'prot_bert' model as backend to make the co
 - pytorch==1.5.0
 
 ## tasks
-- probability
 - transformers
-- unsupervised
+- supervised
+- multi-classification
+- protein-family-prediction
 
 ## embeddings
-- ESM
+- ProtBert (mean)
 
 ## datasets
+
+- `pfam-32.0` dataset
 
--- src/app.py
+++ src/app.py
@@ -2,12 +2,15 @@
 WARNINGS: if you run the app locally and don't have a GPU
           you should choose device='cpu'
 """
-
+from pathlib import Path
 from typing import Dict, List, Optional
 
+import torch
 from biotransformers import BioTransformers
 from deepchain.components import DeepChainApp
 from torch import load
+
+import joblib
 
 Score = Dict[str, float]
 ScoreList = List[Score]
@@ -17,46 +20,72 @@ class App(DeepChainApp):
     """DeepChain App template:
 
     - Implement score_names() and compute_score() methods.
-    - Choose a a transformer available on DeepChain
+    - Choose a a transformer available on BioTranfformers
     - Choose a personal keras/tensorflow model
     """
 
     def __init__(self, device: str = "cuda:0"):
         self._device = device
         self.transformer = BioTransformers(backend="protbert", device=device)
-
-        # TODO: fill _checkpoint_filename if needed
         # Make sure to put your checkpoint file in your_app/checkpoint folder
-        self._checkpoint_filename: Optional[str] = None
+        self._checkpoint_filename: Optional[str] = "family_model.pt"
+        self._checkpoint_label_encoder: Optional[str] = "label_encoder.joblib"
 
-        # TODO:  Use proper loading function
-        # load_model for tensorflow/keras model - load for pytorch model
+        # load_model for tensorflow/keras model-load for pytorch model
         if self._checkpoint_filename is not None:
             self.model = load(self.get_checkpoint_path(__file__))
 
+        # load the family label encoder
+        self.label_encoder = joblib.load(self.get_checkpoint_label_encoder_path(__file__))
+
+    def get_checkpoint_label_encoder_path(self, root_path: str) -> str:
+        """
+        Return solve checkpoint model path
+        Args:
+            root_path : path of the app file launch
+        Raise:
+            FileExistsError if no file are found inside the checkpoint folder
+        """
+        checkpoint_dir = (Path(root_path).parent / "../checkpoint").resolve()
+        path_filename = checkpoint_dir / self._checkpoint_label_encoder
+        if not path_filename.is_file():
+            raise FileExistsError(
+                f"File {self._checkpoint_label_encoder} not found in checkpoint folder."
+                f" Set 'self._checkpoint_filename = None' if file not exists"
+            )
+        return path_filename
+
     @staticmethod
     def score_names() -> List[str]:
-        """App Score Names. Must be specified
+        """App Score Names. Must be specified.
 
         Example:
          return ["max_probability", "min_probability"]
         """
-        # TODO : Put your own score_names here
-        return ["loglikelihood"]
+        return ["protein_family_id"]
 
     def compute_scores(self, sequences: List[str]) -> ScoreList:
-        """Return a list of all proteins score
+        """Return a list of protein family id predictions for the given sequences.
 
-        Score must be a list of dict:
-                - element of list is protein score
-                - key of dict are score_names
+        Args:
+            sequences: sequences for which to predict family ids
         """
-        # TODO : Fill with you own score function
 
-        loglikelihoods = self.transformer.compute_loglikelihood(sequences)
-        log_list = [{self.score_names()[0]: log} for log in loglikelihoods]
+        try:
+            # biotransformers v0.0.3
+            x_embedding = self.transformer.compute_embeddings(sequences, pool_mode=["mean"])["mean"]
+        except:
+            # biotransformers v0.0.2
+            x_embedding = self.transformer.compute_embeddings(sequences, pooling_list=["mean"])["mean"]
 
-        return log_list
+        y_hat = self.model(torch.tensor(x_embedding))
+        predictions = torch.max(y_hat, dim=1)[1]
+        predictions = predictions.detach().cpu().numpy()
+
+        family_predictions = self.label_encoder.inverse_transform(predictions)
+        family_list = [{"protein_family_id": family_pred} for family_pred in family_predictions]
+
+        return family_list
 
 
 if __name__ == "__main__":
